80faf5d6926c39b00dd4aec1c8fa55f8fe54a5bc
JoelCAI <joelargentina12345@gmail.com>
2022-04-16 12:43:33 -0300

    Agregar archivos de proyecto.

---
+++ ArrayListaDiccionario42/Sistema.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListaDiccionario42
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+
+            string frase = Validador.PedirString("\n Ingrese una frase");
+
+            char[] caracter = frase.ToCharArray();
+
+            Array.Reverse(caracter);
+            Console.Clear();
+            Console.WriteLine("\n Usted ingreso la frase : *" + frase + "*.");
+            Console.WriteLine("\n La frase escrita al revés es: \n");
+            foreach (char c in caracter)
+            {
+                Console.Write(c);
+            }
+
+            Console.WriteLine("\n ");
+            Validador.Despedida();
+
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ ArrayListaDiccionario42/Validador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListaDiccionario42
+{
+    public class Validador
+    {
+        public static string PedirString(string mensaje)
+        {
+            string valor;
+            bool valido = false;
+            string mensajeError = "\n El valor no puede ser vacio y tiene que estar dentro del rango solicitado. ";
+
+            do
+            {
+                Console.WriteLine(mensaje);
+                valor = Console.ReadLine();
+
+                if (valor == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine(mensajeError);
+                }
+                else
+                {
+                    valido = true;
+                }
+
+            } while (!valido);
+            return valor;
+
+        }
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+    }
+}
